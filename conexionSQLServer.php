30da33bca503849560b84f9db724c5d5659d8a74
xescnova <xescnova@gmail.com>
2022-01-28 16:55:31 +0100

    login casi funcional

---
+++ conexionSQLServer.php
@@ -0,0 +1,10 @@
+<?php
+
+        $conex= new PDO("sqlsrv:server=sgbdproyectofinal.database.windows.net;database=Tienda","sgbdadmin","pU#ddwiu9P");
+        /*
+        $consulta=$conex->prepare("SELECT * FROM categoria");
+        $consulta->execute();
+        $datos=$consulta->fetchAll(PDO::FETCH_ASSOC);
+        //var_dump($datos);
+*/
+?>
---
+++ procesarLogin.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login  2</title>
+</head>
+<body>
+    <?php
+
+    $us=$_POST['lg_username'];
+    $co=$_POST['lg_password'];
+  //  $_SESSION['nombre']=$us;
+   // $_SESSION['usu']=$_POST['Usuari'];
+    //$_SESSION['cont']=$_POST['Contrasenya'];
+    include "conexionSQLServer.php";
+    $consulta=$conex->prepare("SELECT * FROM Usuario where name='".$us."' AND password='".$co."'");
+    $consulta->execute();
+    $datos=$consulta->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($datos);
+    ?>
+</body>
+</html>
