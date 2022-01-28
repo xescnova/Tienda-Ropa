8f990cc9d2567b5247fe2a98537036f7cb2ef058
Denny <dennybarreirorubio@hotmail.com>
2022-01-28 18:40:59 +0100

    fetchProducto

--- fetchMongo.php
+++ fetchMongo.php
@@ -54,7 +54,8 @@ function fetchLimit($limit)
     }
 }
 
-function fetchCategoria($categoria) {
+function fetchCategoria($categoria)
+{
     require_once 'conexionMongo.php';
     $peticion = $db->find("{categoria: '.$categoria.'}");
 
@@ -85,33 +86,24 @@ function fetchCategoria($categoria) {
     }
 }
 
-function fetchProducto($id_prod) {
+function fetchProducto($id_prod)
+{
     require_once 'conexionMongo.php';
     $peticion = $db->find("{categoria: '.$categoria.'}");
 
     $filtro = array('sku' => $id_prod);
     $peticion = $db->find($filtro);
-
-    $i = 0;
-    foreach ($peticion as $producto) {
-        echo '
-            <div class="col-md-3 col-lg-3 mb-4 text-center">
-                <div class="product-entry border">
-                    <a href="#" class="prod-img">
-                        <img src="' . $producto["imagen"] . '" class="img-fluid" alt="Free html5 bootstrap 4 template">
-                    </a>
-                    <div class="desc">
-                        <h2><a href="#">' . $producto["titulo"] .  '</a></h2>
-                        <span class="price">' . $producto["precio"] . '€</span>
-                    </div>
+    echo '
+        <div class="col-md-3 col-lg-3 mb-4 text-center">
+            <div class="product-entry border">
+                <a href="#" class="prod-img">
+                    <img src="' . $producto["imagen"] . '" class="img-fluid" alt="Free html5 bootstrap 4 template">
+                </a>
+                <div class="desc">
+                    <h2><a href="#">' . $producto["titulo"] .  '</a></h2>
+                    <span class="price">' . $producto["precio"] . '€</span>
                 </div>
             </div>
-        ';
-        if (++$i == $limit) {
-            break;
-        }
-        if ($i == 4) {
-            echo '<div class="w-100"></div>';
-        }
-    }
+        </div>
+    ';
 }
