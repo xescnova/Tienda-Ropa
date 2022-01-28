17bf1d72b345a1460db6d5828daba1c8b6fb2d17
Denny <dennybarreirorubio@hotmail.com>
2022-01-28 18:35:57 +0100

    Co-authored-by: xescnova <xescnova@users.noreply.github.com> Co-authored-by: kamandomar98@gmail.com <kamandomar98@gmail.com>

--- fetchMongo.php
+++ fetchMongo.php
@@ -53,3 +53,34 @@ function fetchLimit($limit)
         }
     }
 }
+
+function fetchCategoria($categoria) {
+    require_once 'conexionMongo.php';
+    $peticion = $db->find("{categoria: '.$categoria.'}");
+
+    $filtro = array('categoria' => $categoria);
+    $peticion = $db->find($filtro);
+
+    $i = 0;
+    foreach ($peticion as $producto) {
+        echo '
+            <div class="col-md-3 col-lg-3 mb-4 text-center">
+                <div class="product-entry border">
+                    <a href="#" class="prod-img">
+                        <img src="' . $producto["imagen"] . '" class="img-fluid" alt="Free html5 bootstrap 4 template">
+                    </a>
+                    <div class="desc">
+                        <h2><a href="#">' . $producto["titulo"] .  '</a></h2>
+                        <span class="price">' . $producto["precio"] . '€</span>
+                    </div>
+                </div>
+            </div>
+        ';
+        if (++$i == $limit) {
+            break;
+        }
+        if ($i == 4) {
+            echo '<div class="w-100"></div>';
+        }
+    }
+}
